4f4325b602e93f5787b19b64c9e8b5c8f714c1ff
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-03 19:47:26 +0530

    exploring get, post & delete

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//* this will only handle the GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Rajnish", lastName: "Pandey" });
+});
+
+app.post("/user", (req, res) => {
+  //* saving data to DB
+  res.send("Data successfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data successfully deleted from database");
+});
+
+//* this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server!");
-});
-
-app.use("/hello/2", (req, res) => {
-  res.send("Hello from the server 2");
-});
-app.use("/hello", (req, res) => {
-  res.send("Hello rounte");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("Home route");
 });
 
 app.listen(3000, () => {
