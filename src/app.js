06a34428da170a02e738d0f5426b8472f6a09f26
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-03 19:10:53 +0530

    play with route

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Home route");
-});
-
 app.use("/test", (req, res) => {
   res.send("Hello from the server!");
 });
 
+app.use("/hello/2", (req, res) => {
+  res.send("Hello from the server 2");
+});
 app.use("/hello", (req, res) => {
   res.send("Hello rounte");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("Home route");
 });
 
 app.listen(3000, () => {
