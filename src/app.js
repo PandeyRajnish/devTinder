8573e85c81d61ef709a3d0e9a158b014890f6b14
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-04 11:00:14 +0530

    exploring middlewares

--- src/app.js
+++ src/app.js
@@ -2,26 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use(
+//* GET /users => middleware chain => request handles
+app.use("/", (req, res, next) => {
+  console.log("Handling the route user");
+  // res.send("Handling the route user")
+  next();
+});
+
+app.get(
   "/user",
   (req, res, next) => {
-    console.log("Handling the route user");
-    // res.send("Response");
     next();
   },
   (req, res, next) => {
-    console.log("Handling the route user 2");
-    // res.send("Response 2");
     next();
   },
   (req, res, next) => {
-    console.log("Handling the route user 3");
-    // res.send("Response 3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4");
-    res.send("Response 4");
+    res.send("Hello from the server!");
   }
 );
 
