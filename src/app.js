03ef12721f719956f0671c7a8387e1576cc360d4
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-04 10:05:30 +0530

    exploring request handlers

--- src/app.js
+++ src/app.js
@@ -2,15 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Rajnish", lastName: "Pandey" });
-});
-
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Rajnish", lastName: "Pandey" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user");
+    // res.send("Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2");
+    // res.send("Response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3");
+    // res.send("Response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4");
+    res.send("Response 4");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
