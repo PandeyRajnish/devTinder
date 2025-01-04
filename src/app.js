a1f4c60c6c7d9ed813d823854a8e49fc400050e0
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-04 11:27:43 +0530

    authorization without middlewares

--- src/app.js
+++ src/app.js
@@ -2,25 +2,26 @@ const express = require("express");
 
 const app = express();
 
-//* GET /users => middleware chain => request handles
-app.use("/", (req, res, next) => {
-  console.log("Handling the route user");
-  // res.send("Handling the route user")
-  next();
+app.get("/admin/getAllData", (req, res) => {
+  // logic for checking if the request is authorized
+  const token = "xklaflyz";
+  const isAdminAuthorized = token === "xyz";
+  if (isAdminAuthorized) {
+    res.send("All data sent!");
+  } else {
+    res.status(401).send("Unauthorized! request");
+  }
 });
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {
-    res.send("Hello from the server!");
+app.get("/admin/deleteUser", (req, res) => {
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (isAdminAuthorized) {
+    res.send("User deleted successfully!");
+  } else {
+    res.status(401).send("Unauthorized! request");
   }
-);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
