5a646fec692cef69bfe816af47b93b63fd4b568d
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-05 16:02:29 +0530

    add validations

--- src/Models/user.js
+++ src/Models/user.js
@@ -7,27 +7,41 @@ const userSchema = new mongoose.Schema(
       required: true,
       minLength: 3,
       maxLength: 50,
+      trim: true,
     },
     lastName: {
       type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+      trim: true,
     },
     emailId: {
       type: String,
       lowercase: true,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
       trim: true,
+      match: [
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+        "Please provide a valid email address",
+      ],
     },
     password: {
       type: String,
       required: true,
+      minLength: 8,
+      maxLength: 16,
+      trim: true,
     },
     age: {
       type: Number,
       min: 18,
+      trim: true,
     },
     gender: {
       type: String,
+      trim: true,
       validate(value) {
         if (!["male", "female", "others"].includes(value)) {
           throw new Error("Gender data is not valid");
@@ -36,11 +50,14 @@ const userSchema = new mongoose.Schema(
     },
     photoUrl: {
       type: String,
+      trim: true,
       default: "https://www.pngall.com/profile-png/",
     },
     about: {
       type: String,
       default: "This is a default about of the user",
+      minLength: 2,
+      maxLength: 50,
     },
     skills: {
       type: [String],
