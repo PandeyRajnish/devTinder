1e0f109d00bbc05fdebf7ad74e23af106d60dd5a
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-05 15:13:24 +0530

    add custom validations

--- src/app.js
+++ src/app.js
@@ -88,10 +88,13 @@ app.patch("/user", async (req, res) => {
   console.log(data);
 
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data);
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
     res.send("User updated successfully!!");
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("UPDATE FAILED: " + err.message);
   }
 });
 
