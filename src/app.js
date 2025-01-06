fec78e55df8eb7a081feb08a367ba3647345974e
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-06 10:59:19 +0530

    create login API

--- src/app.js
+++ src/app.js
@@ -31,6 +31,28 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("Invalid credentials!");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      res.send("Login successful!!!");
+    } else {
+      throw new Error("Invalid credentials!");
+    }
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+});
+
 //* Get user by email
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
