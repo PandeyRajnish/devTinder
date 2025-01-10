2b96fe09609bdd1f0efb761a1217e592644fbf99
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-10 14:08:17 +0530

    remove console.log

--- src/app.js
+++ src/app.js
@@ -50,7 +50,6 @@ app.post("/login", async (req, res) => {
       //* Create a JWT Token
 
       const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790");
-      console.log(token);
 
       //*  Add the tokent to the cookie and send the response back to the user
       res.cookie("token", token);
@@ -74,9 +73,8 @@ app.get("/profile", async (req, res) => {
 
     //* Validate my token
     const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
-    console.log("decodedMessage", decodedMessage);
+
     const { _id } = decodedMessage;
-    console.log("Logged In user is: " + _id);
 
     const user = await User.findById(_id);
     if (!user) {
