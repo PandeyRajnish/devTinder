8edfaf8a7bac088ea1655966a55bbd446b1cf4b8
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-01-10 13:07:15 +0530

    handle error case

--- src/app.js
+++ src/app.js
@@ -64,19 +64,29 @@ app.post("/login", async (req, res) => {
 });
 
 app.get("/profile", async (req, res) => {
-  const cookies = req.cookies;
+  try {
+    const cookies = req.cookies;
 
-  const { token } = cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid Token");
+    }
 
-  //* Validate my token
-  const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
-  console.log("decodedMessage", decodedMessage);
-  const { _id } = decodedMessage;
-  console.log("Logged In user is: " + _id);
+    //* Validate my token
+    const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
+    console.log("decodedMessage", decodedMessage);
+    const { _id } = decodedMessage;
+    console.log("Logged In user is: " + _id);
 
-  const user = await User.findById(_id);
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
 
-  res.send(user);
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
 
 //* Get user by email
