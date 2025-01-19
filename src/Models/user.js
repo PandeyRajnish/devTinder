61a9e0822b3dbb89c000c277bc415249bb3a13ed
Rajnish Kumar Pandey <130439359+RajnishPandeyTeqfocus@users.noreply.github.com>
2025-01-19 15:29:35 +0530

    add ConnectionRequestModel

--- src/Models/user.js
+++ src/Models/user.js
@@ -48,11 +48,15 @@ const userSchema = new mongoose.Schema(
     gender: {
       type: String,
       trim: true,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid");
-        }
+      enum: {
+        values: ["male", "female", "others"],
+        message: "{VALUE} is not a valid gender type",
       },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
     },
     photoUrl: {
       type: String,
